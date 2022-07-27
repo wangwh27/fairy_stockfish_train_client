72d3ba4347702d7f5b12033015ff0a340d92c92e
Vincentzyx <929403983@qq.com>
2022-07-28 03:30:59 +0800

    更灵活的谱数自动调整

--- client.py
+++ client.py
@@ -15,12 +15,13 @@ FILE_NAME_UPLOAD = "xiangqi-upload.bin"
 program_version = "1.2"
 model_version = -1
 Debug = False
-generate_amount = 10000
+generate_amount = -1
 parser = argparse.ArgumentParser(description="分布式生成棋谱")
 parser.add_argument("--user", default="VinXiangQi", type=str, help="用于统计训练量的用户名")
 parser.add_argument("--threads", default=-1, type=int, help="用于跑谱的核心数")
 Args = parser.parse_args()
 generation_params = {}
+last_depth = -1
 NEED_EXIT = False
 
 param_keys_map = {
@@ -125,23 +126,38 @@ def upload_data():
     update_model(result[0], result[1])
 
 
+def get_next_generation_params(speed=None):
+    global generation_params, generate_amount, last_depth
+    if generate_amount == -1:
+        generate_amount = 10000 // pow(2, generation_params["depth"] - 9)
+        generate_amount = (generate_amount // 100) * 100
+        generate_amount = max(generate_amount, 100)
+        last_depth = generation_params["depth"]
+    else:
+        if generation_params["depth"] == last_depth:
+            generate_amount = int(120 * speed)
+            generate_amount = (generate_amount // 100) * 100
+            generate_amount = max(generate_amount, 100)
+        else:
+            generate_amount = -1
+            get_next_generation_params()
+
 if __name__ == "__main__":
     print("-----------------------------------")
     print(f"----- 以 {Args.user} 身份进行训练 -----")
     print("-----------------------------------")
     check_update()
+    get_next_generation_params()
     while True:
         if NEED_EXIT:
             sys.exit(0)
         try:
-            print("开始生成棋谱，该过程耗时较长，请耐心等待……")
+            print(f"开始生成 {generate_amount} 棋谱，该过程耗时较长，请耐心等待……")
             start_time = time.time()
             fairy.generate_data(generation_params, threads=Args.threads, amount=generate_amount)
             time_cost = time.time() - start_time
             speed = generate_amount / time_cost
-            generate_amount = int(120 * speed)
-            generate_amount = (generate_amount // 2000) * 2000
-            generate_amount = max(generate_amount, 2000)
+            get_next_generation_params(speed)
             print("生成完成！耗时: {0}s, 下次生成预计生成 {1} 棋谱".format(round(time_cost, 1), generate_amount))
             if not os.path.exists(FILE_NAME):
                 print("棋谱文件不存在，上传失败！")
